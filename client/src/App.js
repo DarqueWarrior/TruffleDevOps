364a00624c857216d2fd405a87d3c91a83483138
Donovan Brown <dbrown@microsoft.com>
2022-06-05 05:26:26 +0000

    externalized address

--- client/src/App.js
+++ client/src/App.js
@@ -18,9 +18,21 @@ class App extends Component {
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = SimpleStorageContract.networks[networkId];
+
+      let contractAddress = deployedNetwork && deployedNetwork.address;
+
+      // If the network can't be found in the contract JSON call the
+      // backend API for the address.
+      if (!contractAddress) {
+        console.log('Address not found in contract JSON. Calling backup api');
+        const text = await (await fetch(`/api/GetContractAddress/?networkId=${networkId}`)).text();
+        console.log(`API returned: ${text}`);
+        contractAddress = text;
+      }
+
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        contractAddress,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,11 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
     }
   },
   mocha: {
