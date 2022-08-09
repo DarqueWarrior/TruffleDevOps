ee68baa1e433085c2c466498336a765f653beadf
Donovan Brown <dbrown@microsoft.com>
2022-08-09 21:43:49 +0000

    deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,24 @@
 const path = require("path");
 require('dotenv').config();
 var devNetworkHost = process.env["DEV_NETWORK"];
+var apiKey = process.env["API_KEY"];
+var mnemonic = process.env["NEMONIC"];
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
 config = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
+    rinkeby: {
+      host: "localhost",
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + apiKey);
+      },
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000
+    },
     develop: {
       port: 8545
     },
